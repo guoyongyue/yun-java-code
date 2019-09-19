84136f775556a147f51ad284b14fc1d657097bbc
guoyongyue <guoyongyue.charles@shandagames.com>
2019-09-19 20:30:06 +0800

    xiug

--- src/main/java/com/yun/util/Test.java
+++ src/main/java/com/yun/util/Test.java
@@ -2,6 +2,6 @@ package com.yun.util;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(8&536870912);
+        System.out.println(536870902&536870912);
     }
 }
