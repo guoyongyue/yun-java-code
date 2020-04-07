f306c45a7494332073d34fefd8b144e525869199
guoyongyue <guoyongyue.charles@shandagames.com>
2020-04-07 19:16:35 +0800

    add
    add

---
+++ src/main/java/com/yun/code/day08/S78子集.java
@@ -0,0 +1,15 @@
+package com.yun.code.day08;
+
+import java.util.List;
+
+public class S78子集 {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+    }
+}
--- src/main/java/com/yun/demo/EventTest.java
+++ src/main/java/com/yun/demo/EventTest.java
@@ -1,35 +1,19 @@
 package com.yun.demo;
 
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-import java.util.concurrent.*;
-
 // main 方法
 public class EventTest {
-
-
     public static void main(String[] args) {
 
-        BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>();
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(6,6,60,TimeUnit.SECONDS,workQueue);
-        for (int i = 0; i < 10000; i++) {
-            threadPoolExecutor.execute(new MyRunnable(i));
+        if(1 !=3 && 1 !=15 ){
+            System.out.println("-");
+        }else {
+            System.out.println("-----df");
         }
-
         // 初始化发布者实体类
-//        PublishService publishService = new PublishService();
+        PublishService publishService = new PublishService();
         // 发送消息
-//        publishService.sendMessage();
+        publishService.sendMessage();
     }
 
-    static class MyRunnable implements Runnable{
-        int i=0;
-        public MyRunnable(int i){
-            this.i=i;
-        }
-        @Override
-        public void run() {
-            System.out.println(Thread.currentThread().getName()+" run i:" + i);
-        }
-    }
+
 }
