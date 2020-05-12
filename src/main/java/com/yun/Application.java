b5248769915026d9c875f5e2f1e41a9b80290673
guoyongyue <guoyongyue.charles@shandagames.com>
2020-05-12 08:30:40 +0800

    add
    add

--- src/main/java/com/yun/Application.java
+++ src/main/java/com/yun/Application.java
@@ -1,15 +1,35 @@
 package com.yun;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 
+		List<String> list = new ArrayList<String>();
+		list.add("");
+		list.add("");
+		list.add("");
+		list.add("1");
+		list.add("2");
+		list.add("3");
+
+		String messageKey = FastJsonUtils.convertObjectToJSON(list);
+		List<String> messageKeys = FastJsonUtils.toList(messageKey, String.class);
+		System.out.println(messageKeys);
+
+
 		System.out.println(Double.valueOf("12.12312412412512").intValue());
-		SpringApplication.run(Application.class, args);
+//		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/main/java/com/yun/code/util/ArrayUtil.java
+++ src/main/java/com/yun/code/util/ArrayUtil.java
@@ -12,27 +12,7 @@ public class ArrayUtil {
         System.out.println(">>>>>>>>>>> project begin");;
     }
 
-    //掩码姓名
-    public static String maskName(String name) {
-        if (name == null || name.length() < 2) {
-            return null;
-        }
-        if (name.length() == 2) {
-            name = name.substring(0, 1) + "*";
-        } else {
-            name = name.replaceAll(name.substring(1, name.length() - 1), "*");
-        }
-        return name;
-    }
 
-    //掩码手机号
-    public static String maskPhone(String tel) {
-        if (tel == null || tel.length() < 11) {
-            return null;
-        }
-        tel = tel.replaceAll("(\\d{3})\\d{4}(\\d{4})", "$1****$2");
-        return tel;
-    }
 
 
     //旋转二维数值从行格式变为列格式
