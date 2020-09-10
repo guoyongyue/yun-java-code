3e2968dfcfa70419efcb663fbcedb7c375ff226c
guoyongyue <guoyongyue.charles@shandagames.com>
2020-09-10 16:07:53 +0800

    add
    add

--- java-code/src/main/java/com/yun/algorithm/tree/Fibonacci.java
+++ java-code/src/main/java/com/yun/algorithm/tree/Fibonacci.java
@@ -2,6 +2,9 @@ package com.yun.algorithm.tree;
 
 import com.yun.util.StringUtil;
 
+import java.util.ArrayList;
+import java.util.Vector;
+
 /**
  * @author: yun<\br>
  * @description: <\br>
@@ -11,8 +14,12 @@ public class Fibonacci {
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
         StringUtil.printlnBegin("迭代打印斐波那契数列");
-        StringUtil.printlnRet(fibonacci.fib(3));
+        StringUtil.printlnRet(fibonacci.fib(5));
         StringUtil.printlnEnd("迭代打印斐波那契数列");
+
+        StringUtil.printlnBegin("迭代打印斐波那契数列 剪枝法");
+        StringUtil.printlnRet(fibonacci.fib2(5));
+        StringUtil.printlnEnd("迭代打印斐波那契数列 剪枝法");
 
     }
 
@@ -22,4 +29,27 @@ public class Fibonacci {
         }
         return fib(n-1)+fib(n-2);
     }
+
+
+    private int fib2(int n){
+        Vector<Integer> memo = new Vector<>(16);
+        memo.add(1);
+        memo.add(1);
+        return helper(memo, n);
+    }
+
+    private int helper(Vector<Integer> memo,int n){
+        if(n==1 || n==2){
+            return 1;
+        }
+        if(memo.size()<n){
+            int i = helper(memo, n - 1) + helper(memo, n - 2);
+            memo.add(i);
+            return i;
+        }else {
+            return memo.get(n-1);
+        }
+    }
+
+
 }
