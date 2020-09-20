653a59e4afbbe8b354b34452ceb072908026d5a2
toku <guoyongyue1996@163.com>
2020-09-20 21:37:47 +0800

    Update S95不同的二叉搜索树II.java
    add

--- java-code/src/main/java/com/yun/code/day10/S95不同的二叉搜索树II.java
+++ java-code/src/main/java/com/yun/code/day10/S95不同的二叉搜索树II.java
@@ -10,6 +10,8 @@ import java.util.List;
  */
 public class S95不同的二叉搜索树II {
     public static void main(String[] args) {
+        S95不同的二叉搜索树II s95不同的二叉搜索树II = new S95不同的二叉搜索树II();
+        s95不同的二叉搜索树II.generateTrees(1);
     }
 
     public List<TreeNode> generateTrees(int n) {
